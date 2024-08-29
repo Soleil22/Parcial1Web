74a7ddd5b9f9aecd83a160e516bf075b35fbef4d
Soleil <solangegarciaguamanga@gmail.com>
2024-08-29 18:55:28 -0500

    mejorando estructura

--- src/components/inputOnlyOption/inputOnlyOption.jsx
+++ src/components/inputOnlyOption/inputOnlyOption.jsx
@@ -3,16 +3,15 @@ import './inputOnlyOption.css'
 function InputOnlyOption(props) {
     return (
       <>
+    <div class="container">
       <h1>{props.tituloCate}</h1>
-        <div>
             <h3>{props.text}</h3>
             <p>{props.price}</p>
-            <img src={props.img} alt="" srcset="" />
-        </div>
+            <img class="imagen" src={props.img} alt="" srcset="" />
         <label htmlFor="">
             <input type="radio" />
         </label>
-
+        </div>
       </>
     )
   }
